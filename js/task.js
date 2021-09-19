4ed351e0087326a1c38d07fcd4e05560955fc373
ElenaStepanchuk <podolyak365@gmail.com>
2021-09-19 17:45:26 +0300

    new 1

--- js/task.js
+++ js/task.js
@@ -1,14 +1,55 @@
-// Change code below this line
-function findMatches(argsArray, ...args) {
-  const matches = []; // Don't change this line
-  for (let i = 0; i < argsArray.length; i += 1) {
-    if (argsArray.includes(args[i])) {
-      matches.push(args[i]);
+// // Change code below this line
+// function findMatches(argsArray, ...args) {
+//   const matches = []; // Don't change this line
+//   for (let i = 0; i < argsArray.length; i += 1) {
+//     if (argsArray.includes(args[i])) {
+//       matches.push(args[i]);
+//     }
+//   }
+//   // Change code above this line
+//   return matches;
+// }
+// console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
+// console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 9, 5));
+// console.log(findMatches([1, 2, 3, 4, 5], 0, 8, 9, 7));
+
+
+
+const atTheOldToad = {
+  potions: [
+    { name: "Speed potion", price: 460 },
+    { name: "Dragon breath", price: 780 },
+    { name: "Stone skin", price: 520 },
+  ],
+  // Change code below this line
+  getPotions() {
+    return this.potions;
+  },
+  addPotion(newPotion) {
+    if (this.potions.includes(newPotion)) {
+      return `Error! Potion ${newPotion} is already in your inventory!`;
     }
-  }
+
+    this.potions.push(newPotion);
+  },
+  removePotion(potionName) {
+    const potionIndex = this.potions.indexOf(potionName);
+
+    if (potionIndex === -1) {
+      return `Potion ${potionName} is not in inventory!`;
+    }
+
+    this.potions.splice(potionIndex, 1);
+  },
+  updatePotionName(oldName, newName) {
+    const potionIndex = this.potions.indexOf(oldName);
+
+    if (potionIndex === -1) {
+      return `Potion ${oldName} is not in inventory!`;
+    }
+
+    this.potions.splice(potionIndex, 1, newName);
+  },
   // Change code above this line
-  return matches;
-}
-console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
-console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 9, 5));
-console.log(findMatches([1, 2, 3, 4, 5], 0, 8, 9, 7));
+};
+console.log(atTheOldToad.addPotion({ name: "Invisibility", price: 620 }));
