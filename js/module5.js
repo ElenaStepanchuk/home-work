9158f732c1d2e18bcebc6d19a346f2c82715813a
ElenaStepanchuk <podolyak365@gmail.com>
2021-10-15 13:04:23 +0300

    all task

--- js/module5.js
+++ js/module5.js
@@ -400,5 +400,60 @@
 // console.log(audi.price); // 49000
 
 
-// task 17
+// task 20
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   // Change code below this line
+//   constructor({ email, accessLevel }) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//     this.blacklistedEmails = [];
+//   }
+  
+
+// blacklist(email) {
+// return this.blacklistedEmails.push(email);
+// }
+//   isBlacklisted(email) {
+//     if (this.blacklistedEmails.includes(email)) {
+//     return true;
+//     } return false;
+//   }
+
+//   static AccessLevel = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+  
+//   // Change code above this line
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   accessLevel: Admin.AccessLevel.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.accessLevel); // "superuser"
+
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
+
 
